9e9e4699aeb27a5475d1135843ee52aacb26d84d
ivan <ivanvinicius@github.com>
2021-03-14 10:02:05 -0300

    feat: initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+export const App = (): React.ReactNode => {
+  return (
+    <View>
+      <Text>This is working really well</Text>
+    </View>
+  );
+};
