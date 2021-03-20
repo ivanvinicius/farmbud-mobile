cc0e66f80c291b0f0fa00ab322123043ee3be56b
ivan <ivanvinicius@github.com>
2021-03-20 14:29:40 -0300

    feat: Cultures page

--- src/pages/Cultures/index.tsx
+++ src/pages/Cultures/index.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import {useNavigation} from '@react-navigation/native';
 
 import {PageTitle} from '../../components/PageTitle';
+import {api} from '../../services/api';
 
-import {Container} from './styles';
+import {Container, List, ListItem, ListItemDescription} from './styles';
+
+export interface ICultureProps {
+  id: string;
+  name: string;
+}
 
 export function Cultures() {
+  const [cultures, setCultures] = useState<ICultureProps[]>([]);
+  const {navigate} = useNavigation();
+
+  useEffect(() => {
+    api.get('/cultures').then((response) => setCultures(response.data));
+  }, []);
+
   return (
     <Container>
-      <PageTitle title="Culturas" navigateBack navigateTo="s" />
+      <PageTitle title="Culturas" navigateBack />
+
+      <List
+        data={cultures}
+        keyExtractor={(culture) => culture.id}
+        renderItem={({item: culture}) => (
+          <ListItem onPress={() => navigate('Dashboard')}>
+            <ListItemDescription>{culture.name}</ListItemDescription>
+          </ListItem>
+        )}
+      />
     </Container>
   );
 }
--- src/pages/Cultures/styles.ts
+++ src/pages/Cultures/styles.ts
@@ -1,5 +1,31 @@
 import styled from 'styled-components/native';
+import {FlatList} from 'react-native';
+import {TouchableOpacity} from 'react-native-gesture-handler';
+
+import {ICultureProps} from './index';
 
 export const Container = styled.View`
   flex: 1;
+`;
+
+export const List = styled(FlatList as new () => FlatList<ICultureProps>)``;
+
+export const ListItem = styled(TouchableOpacity)`
+  height: 60px;
+  margin-bottom: 16px;
+  padding: 8px;
+
+  justify-content: center;
+
+  background-color: #f8f8fc;
+  border: 1px solid #e6e6f0;
+  border-radius: 8px;
+
+  box-shadow: 6px 6px 9px rgba(0, 0, 0, 0.2);
+`;
+
+export const ListItemDescription = styled.Text`
+  font-family: 'Poppins-Regular';
+  font-size: 16px;
+  color: #7620d8;
 `;
